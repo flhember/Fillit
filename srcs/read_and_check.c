22a2ee0da2ec98b7a927fbbce927b748b817a1af
Bruno PINTO <brpinto@e3r8p19.42.fr>
2019-01-09 18:26:57 +0100

    gestion du type de caracteres dans le maillon

--- srcs/read_and_check.c
+++ srcs/read_and_check.c
@@ -6,7 +6,7 @@
 /*   By: flhember <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 16:33:49 by flhember          #+#    #+#             */
-/*   Updated: 2019/01/09 16:32:45 by flhember         ###   ########.fr       */
+/*   Updated: 2019/01/09 18:24:50 by brpinto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,35 @@
 
 int		ft_check_char(char *tetri)
 {
+	// mauvais retour si line_width < 3
 	int		i;
+	int		line_width;
 
 	i = 0;
-	while (tetri[i])
+	line_width = 0;
+	while (tetri[i] != '\0')
 	{
-		if (tetri[i] == '.' && tetri[i] == '#')
-			i++;
-		else if ((tetri[i] == '\n' && (tetri[i - 1] == '.')) ||
-				(tetri[i - 1] == '#'))
-			i++;
-		else if (tetri[i] == '\n' && tetri[i - 1] == '\n')
-			i++;
-		else
-			return (-1);
+		while (tetri[i] != '\n')
+		{
+			if (tetri[i] == '.' || tetri[i] == '#' || tetri[i] == '\n')
+			{
+				if (line_width > 3)
+					return (-1);
+				ft_putnbr(line_width);
+				ft_putchar ('\n');
+				line_width++;
+				i++;
+			}
+			else
+				return (-1);
+		}
+		line_width = 0;
+		i++;
 	}
-	return (0);
+	return (1);
 }
 
-int		ft_check_tetrie(char *tetrie)
+/* int		ft_check_tetrie(char *tetrie)
 {
 	int		i;
 
@@ -53,25 +63,28 @@ int		ft_check_tetri(char *tetrie)
 	if (!(ft_check_tetrie(tetrie)))
 		return (-1);
 	return (0);
-}
+} */
 
 t_list		*ft_read_file(int fd, t_list **list)
 {
 	int		ret;
 	char	buf[21];
 	t_list	*tmp;
-	//t_list	*tetrie;
 
 	tmp = *list;
 	tmp = ft_lstnew("\0", 1);
 	while ((ret = read(fd, buf, 21)))
 	{
 		buf[ret] = '\0';
-		if ((!ft_check_tetri(buf)))
-		{
-			ft_putendl_fd("Not a valid file", 2);
-			return (NULL);
-		}	
+		ft_putendl(tmp->content);
+		ft_check_char(tmp->content);
+		ft_putnbr(ft_check_char(tmp->content));
+		ft_putchar('\n');
+	//	if ((!ft_check_tetri(buf)))
+	//	{
+	//		ft_putendl_fd("Not a valid file", 2);
+	//		return (NULL);
+	//	}	
 		tmp = ft_lstnew(buf, ft_strlen(buf));
 		ft_lstadd(list, tmp);
 	}
