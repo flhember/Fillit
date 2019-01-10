d2a96a5a5cea57fcf41c24c8ee283ec011a091ff
Florian HEMBERT <flhember@e3r8p4.42.fr>
2019-01-10 17:40:44 +0100

    parsing tetri o

--- srcs/read_and_check.c
+++ srcs/read_and_check.c
@@ -6,53 +6,67 @@
 /*   By: flhember <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 16:33:49 by flhember          #+#    #+#             */
-/*   Updated: 2019/01/09 16:32:45 by flhember         ###   ########.fr       */
+/*   Updated: 2019/01/10 17:36:26 by flhember         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
 
-int		ft_check_char(char *tetri)
+int		ft_check_tetri(char *tetri)
 {
 	int		i;
+	int		nb;
 
+	nb = 0;
 	i = 0;
 	while (tetri[i])
 	{
-		if (tetri[i] == '.' && tetri[i] == '#')
-			i++;
-		else if ((tetri[i] == '\n' && (tetri[i - 1] == '.')) ||
-				(tetri[i - 1] == '#'))
-			i++;
-		else if (tetri[i] == '\n' && tetri[i - 1] == '\n')
-			i++;
-		else
-			return (-1);
+		if (tetri[i] == '#')
+			nb++;
+		i++;
 	}
-	return (0);
+	if (nb != 4)
+		return (-1);
+	return (1);
 }
 
-int		ft_check_tetrie(char *tetrie)
+int		ft_check_tetri_valid(char *tetri)
 {
 	int		i;
+	int		j;
 
+	j = 0;
 	i = 0;
-	while (tetrie[i] != '#')
+	while (tetri[i])
 	{
+		if (tetri[i] == '#')
+		{
+			if (tetri[i - 1] == '#')
+				j++;
+			if (tetri[i - 5] == '#')
+				j++;
+			if (tetri[i + 1] == '#')
+				j++;
+			if (tetri[i + 5] == '#')
+				j++;
+		}
 		i++;
-
 	}
-
-	return (0);
+	printf("j = %d\n", j);
+	if (j < 6)
+		return (-1);
+	return (1);
 }
 
-int		ft_check_tetri(char *tetrie)
+int		ft_check(char *tetri)
 {
-	if (!(ft_check_char(tetrie)))
+	//if (!(ft_check_char(tetri)))
+	//	return (-1);
+	if (!(ft_check_tetri(tetri)))
 		return (-1);
-	if (!(ft_check_tetrie(tetrie)))
+	if (!(ft_check_tetri_valid(tetri)))
 		return (-1);
-	return (0);
+	return (1);
 }
 
 t_list		*ft_read_file(int fd, t_list **list)
@@ -60,18 +74,18 @@ t_list		*ft_read_file(int fd, t_list **list)
 	int		ret;
 	char	buf[21];
 	t_list	*tmp;
-	//t_list	*tetrie;
 
 	tmp = *list;
 	tmp = ft_lstnew("\0", 1);
 	while ((ret = read(fd, buf, 21)))
 	{
 		buf[ret] = '\0';
-		if ((!ft_check_tetri(buf)))
+		if ((!ft_check(buf)))
 		{
 			ft_putendl_fd("Not a valid file", 2);
 			return (NULL);
-		}	
+		}
+		printf("ouai ouai\n");
 		tmp = ft_lstnew(buf, ft_strlen(buf));
 		ft_lstadd(list, tmp);
 	}
